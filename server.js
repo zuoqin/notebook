2a37d7db68031d17d6ff42bbdb785b9dc946ac2b
Alexey Zorchenkov <zuoqinr@163.com>
2015-08-22 10:55:10 +0800

    CORS ADDED

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.static(__dirname + '/public'));
 
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 var api = require('./app/routes/api')(app,express);
 app.use('/api', api);
 
